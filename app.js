694acf25c0af0ebaa40f1a405235fa173e753c31
PrideOfConcept <info@PrideOfConcept.com>
2018-07-19 07:01:53 -0400

    code cleanup

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const spawn = require('child_process').spawn;
-var firebase = require("firebase");
+const firebase = require("firebase");
 const admin = require('firebase-admin');
+const chokidar = require('chokidar');
 
 let child = null //spawn('python', ['cam_record.py', 'media/']);
 
-var serviceAccount = require('./key/serviceAccount.json');
+const serviceAccount = require('./key/serviceAccount.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-var db = admin.firestore();
-const doc = db.collection('camera').doc('scoopcam1') // todo: s houdl the collections be exported from the firebase module
+const db = admin.firestore();
+const doc = db.collection('camera').doc('scoopcam1'); // todo: should the collections be exported from the firebase module
 
 let isCameraOn = false;
 
 const handleCurrentCameraUpdate = (docSnapshot) => {
     //console.log(docSnapshot.id, '=>', docSnapshot.data());
-    const data = docSnapshot.data()
+    const data = docSnapshot.data();
     if(data.onRide) {
-        console.log('we are on ride')
+        console.log('we are on ride');
         if(!isCameraOn){
-            console.log('turning camera ON!')
+            console.log('turning camera ON!');
             //child = spawn('python', ['cam_record.py', 'media/']);
             isCameraOn = true;
         }
     } else {
-        console.log('we are NOT riding')
+        console.log('we are NOT riding');
         if(child && isCameraOn){
-            console.log('turning off camera')
+            console.log('turning off camera');
             child.kill();
             isCameraOn = false;
         }
@@ -45,5 +46,5 @@ const setupListeners = () => {
         (error) => { console.log("Error getting documents: ", error);})
 }
 
-loadCUrrentFirestoreData()
+loadCUrrentFirestoreData();
 setupListeners();
