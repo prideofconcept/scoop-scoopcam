a935abd95078cbe6ab5f09d56ee40852f88ad42b
PrideOfConcept <info@PrideOfConcept.com>
2018-07-19 08:41:56 -0400

    state in root obj, on newfile function started

--- app.js
+++ app.js
@@ -15,28 +15,34 @@ admin.initializeApp({
 const db = admin.firestore();
 const doc = db.collection('camera').doc('scoopcam1'); // todo: should the collections be exported from the firebase module
 
-let isCameraOn = false;
+const root = {
+    isCameraOn: false,
+    currRideId: null
+}
 const rideCleanUp = () => {
-    //todo : clear media dir?
     rimraf('.media/*', function() {console.log('...clean, ready.')})
 }
 const handleCurrentCameraUpdate = (docSnapshot) => {
     //console.log(docSnapshot.id, '=>', docSnapshot.data());
+
     const data = docSnapshot.data();
-    if(data.onRide) {
-        console.log('we are on ride');
-        if(!isCameraOn){
+    root.isOnRide = data.onRide;
+    root.currRideId = data.currRide;
+
+    if(root.isOnRide) {
+        console.log('we are on ride', root.currRideId);
+        if(!root.isCameraOn){
             console.log('turning camera ON!');
-            child = spawn('python', ['cam_record.py', 'media/']);
-            isCameraOn = true;
+            child = spawn('python', ['cam_record.py', root.currRideId]);
+            root.isCameraOn = true;
         }
     } else {
         console.log('we are NOT riding');
-        if(child && isCameraOn){
+        if(child && root.isCameraOn){
             console.log('turning off camera');
-            rideCleanUp()
+            rideCleanUp();
             child.kill();
-            isCameraOn = false;
+            root.isCameraOn = false;
         }
     }
 }
@@ -46,15 +52,24 @@ const loadCUrrentFirestoreData = () => {
         .then(handleCurrentCameraUpdate)
 }
 
+const onNewMediaFile = (path) => {
+    if(path.includes('.log')){
+        console.log('just a log file ... skipping')
+        return
+    }
+
+    //upload to bucket
+    console.log('uploading ',path, 'to', root.currRideId);
+    //console.log()
+}
+
 const setupListeners = () => {
     //firestore camera doc changes
     doc.onSnapshot(handleCurrentCameraUpdate,
         (error) => { console.log("Error getting documents: ", error);})
 
     //media directory changes
-    chokidar.watch('media/', {ignored: /(^|[\/\\])\../}).on('add', (path, event) => {
-        console.log(path);
-    });
+    chokidar.watch('media/', {ignored: /(^|[\/\\])\../}).on('add', onNewMediaFile);
 }
 
 rideCleanUp();
