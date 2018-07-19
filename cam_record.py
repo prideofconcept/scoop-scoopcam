e69ba9a1bffa164151272e97c3f7996d31887e1b
PrideOfConcept <info@PrideOfConcept.com>
2018-07-19 08:15:13 -0400

    python script - detect Mac, hardened to errors

--- cam_record.py
+++ cam_record.py
@@ -1,26 +1,42 @@
 import sched, time, sys
-import picamera
+import platform
 
-print("This line will be printed.")
-f= open("media/log.txt","w+")
+def isPiCamera():
+    return  platform.system().find("Darwin") <= -1
+
+if isPiCamera():
+    import picamera
+
+print("startup." + str(platform.system()))
+f = None
+try:
+    #todo: integrate this ride id into save routine
+    ride = sys.argv[1]
+    f = open("media/" + ride + ".log","w+")
+except:
+    print("an error occuried getting arg or opening file")
+
 starttime=time.time()
 
-#todo: integrate this ride id into save routine
-ride = sys.argv[1]
+def start_record():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (800, 600)
+        camera.framerate = 24
+        i = 1
+        camera.start_recording('media/%d.h264' % i)
+
+        #sched
+        while True:
+            print "tick"
+            i = i+1
+            camera.wait_recording(10)
+            camera.split_recording('media/%d.h264' % i)
 
 #init
-with picamera.PiCamera() as camera:
-    camera.resolution = (800, 600)
-    camera.framerate = 24
-    i = 1
-    camera.start_recording('media/%d.h264' % i)
-
-    #sched
-    while True:
-        print "tick"
-        i = i+1
-        camera.wait_recording(10)
-        camera.split_recording('media/%d.h264' % i)
+if isPiCamera():
+    start_record()
 
 #exit stuff
-f.close()
+print("closing down")
+if f != None:
+    f.close()
