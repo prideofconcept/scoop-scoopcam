3fd08fa3b8ee6bbe5930e3f064996d3c2d8d3af9
PrideOfConcept <info@PrideOfConcept.com>
2018-07-19 05:42:52 -0400

    fleshed out app -> pythin cam script

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 const spawn = require('child_process').spawn;
+var firebase = require("firebase");
+const admin = require('firebase-admin');
 
-const child = spawn('python', ['cam_record.py', 'media/']);
+let child = null //spawn('python', ['cam_record.py', 'media/']);
 
+var serviceAccount = require('./key/serviceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+const doc = db.collection('camera').doc('scoopcam1') // todo: s houdl the collections be exported from the firebase module
+
+//loadCUrrentFirestoreData()
+setupListeners();
+
+function loadCUrrentFirestoreData() {
+    doc.get()
+        .then(handleCameraUpdate)
+}
+
+function setupListeners(){
+    doc.onSnapshot(handleCameraUpdate,
+            (error) => { console.log("Error getting documents: ", error);})
+}
+
+function handleCameraUpdate(docSnapshot){
+    //console.log(docSnapshot.id, '=>', docSnapshot.data());
+    const data = docSnapshot.data()
+    if(data.onRide) {
+        console.log('we are on ride')
+        child = spawn('python', ['cam_record.py', 'media/']);
+    } else {
+        console.log('turning off camera')
+        if(child){
+            child.kill();
+        }
+    }
+}
+/*
 setTimeout(function(){
     console.log('kill');
-    child.kill();
-}, 3000);
+
+}, 3000);*/
--- cam_record.py
+++ cam_record.py
@@ -1,16 +1,26 @@
 import sched, time, sys
+import picamera
 
 print("This line will be printed.")
-#file stuff
-save_location = sys.argv[0]
-f= open("media/guru99.txt","w+")
+f= open("media/log.txt","w+")
 starttime=time.time()
 
-#sched
-while True:
-    print "tick"
-    f.write("tick")
-    time.sleep(2.0 - ((time.time() - starttime) % 2.0))
+#todo: integrate this ride id into save routine
+ride = sys.argv[1]
+
+#init
+with picamera.PiCamera() as camera:
+    camera.resolution = (800, 600)
+    camera.framerate = 24
+    i = 1
+    camera.start_recording('media/%d.h264' % i)
+
+    #sched
+    while True:
+        print "tick"
+        i = i+1
+        camera.wait_recording(10)
+        camera.split_recording('media/%d.h264' % i)
 
 #exit stuff
 f.close()
